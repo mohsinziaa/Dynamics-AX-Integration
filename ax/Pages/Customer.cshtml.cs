819d741d71e6f8bead37c77bfff1df2fc81026fc
mohsinziaa <ziamohsin8@gmail.com>
2025-02-11 16:00:40 +0500

    add: added customer data

---
+++ ax/Pages/Customer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ax.Pages
+{
+    public class CustomerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
