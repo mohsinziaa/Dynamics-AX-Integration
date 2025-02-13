8f3ebeb5e2bfe61d94ca3c416045106802376c4d
mohsinziaa <ziamohsin8@gmail.com>
2025-02-13 14:40:50 +0500

    add: added customer checkout

--- ax/Pages/Checkout.cshtml.cs
+++ ax/Pages/Checkout.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace ax.Pages
 {
+    [IgnoreAntiforgeryToken]
     public class CheckoutModel : PageModel
     {
+        private readonly ILogger<CheckoutModel> _logger;
+
+        public CheckoutModel(ILogger<CheckoutModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+            Console.WriteLine("This is from GET");
+        }
+
+        
+        public IActionResult OnPostTest()
+        {
+            Console.WriteLine("This is from POST");
+            return new JsonResult(new { message = "Hello from POST!" }); // Ensure a response is returned
         }
     }
 }
